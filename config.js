1bf291cb32dd87a7e43670b3ffeeee14b7c3f676
wasjediknight <wasjediknight@gmail.com>
2026-03-31 23:32:28 -0300

    V0 - appvitorla

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 window.SPOTIFY_CONFIG = {
   clientId: "8e2b5efe532f435f92b649f8bbdc292e",
-  redirectUri: "https://wasjediknight.github.io/appvitrola/"
+  redirectUri: "https://wasjediknight.github.io/appvitrola/",
+  scopes: [
+    "user-read-private"
+  ]
 };
